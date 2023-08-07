8c5a5f165d205ff249c06b90b17e16b805d1eba7
Oleg Arbuzov <oleg.arbuzov@rt.ru>
2023-08-07 22:35:47 +0300

    устанавливает http-status-codes / добавляет ToastContainer в index

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { offer } from './mocks/offer';
 import { reviews } from './mocks/rewiews';
 import { Provider } from 'react-redux';
 import { store } from './store/index';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,6 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ToastContainer />
       <App
         offer={offer}
         reviews={reviews}
